5cdd8b3eb9825359a8edeab9945da7b73c2bbf21
chensuyue <suyue.chen@intel.com>
2026-06-02 22:08:40 +0800

    bug fix
    Signed-off-by: chensuyue <suyue.chen@intel.com>

--- .buildkite/scripts/test-check-xpu.sh
+++ .buildkite/scripts/test-check-xpu.sh
@@ -8,7 +8,8 @@ echo "NODE_LABEL is $NODE_LABEL"
 echo "Hello world" && sleep 20 && echo "End."
 CONTAINER_NAME=vllm-test-container-$NODE_LABEL
 IMAGE_NAME=vllm-xpu-test:py312
-docker run -tid --disable-content-trust --privileged --name $CONTAINER_NAME -v ${{ github.workspace }}:/workspace -w /workspace $IMAGE_NAME
+REPO_PATH="${BUILDKITE_BUILD_CHECKOUT_PATH:-$PWD}"
+docker run -tid --disable-content-trust --privileged --name "$CONTAINER_NAME" -v "$REPO_PATH:/workspace" -w /workspace "$IMAGE_NAME"
 docker exec -e NUMA_NODE=${NUMA_NODE} -e NUMA_CPUSET=${NUMA_CPUSET} -e ZE_AFFINITY_MASK=${ZE_AFFINITY_MASK} $CONTAINER_NAME \
               bash -c "env | grep -E 'NUMA_NODE|NUMA_CPUSET|ZE_AFFINITY_MASK' && \
                        echo Hello world && sleep 20 && echo End."
